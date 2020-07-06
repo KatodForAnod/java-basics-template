78cbd2435fa7c14d852faa9832f294813360d028
Nikita Makarchuk <vfrfrfcbys@yandex.by>
2020-07-06 13:00:18 +0300

    Add maxFrom realisation

--- src/main/java/com/epam/izh/rd/online/service/SimpleMathService.java
+++ src/main/java/com/epam/izh/rd/online/service/SimpleMathService.java
@@ -29,6 +29,11 @@ public class SimpleMathService implements MathService {
      */
     @Override
     public int maxFrom(int value1, int value2) {
+        if (value1 > value2) {
+            return value1;
+        } else if (value2 >= value1) {
+            return value2;
+        }
         return -1;
     }
 
